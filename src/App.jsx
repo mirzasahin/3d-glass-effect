10529e2027a2efdeca064afca1aa69e21b0e004f
Mirza Sahin <salihmirza123@gmail.com>
2024-12-27 00:44:41 +0300

    Implement 3d glass object to web browser

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Scene from "./Components/Scene";
+
+function App() {
+
+  return (
+    <main className="relative h-screen">
+      <Scene />
+    </main>
+  );
+}
+
+export default App;
